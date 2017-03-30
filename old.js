683dc9567572e51ac05b3fb88021acca1059f69d
Julian Affrime <julian.affrime@gmail.com>
2017-03-30 15:41:44 +0300

    cleaned up code, still only 2^n

--- old.js
+++ old.js
@@ -1,95 +1,21 @@
-var randomArray = Array.from({length: 16}, () => Math.floor(Math.random() * 20) + 1);
-
-console.log("Random Array = ", randomArray);
-
-var givenArray = [5,3,6,1,2,4,8,7];
-
-// console.time("factorial test");
-
-// NOTE: selection sort
-// for (let i=0;i<a.length;i++){
-//   let iMin = i;
-//   for (let j=i+1;j<a.length;j++){
-//     if (a[j]<a[iMin]){
-//       iMin = j;
-//     }
-//   }
-//   let temp = a[i];
-//   a[i] = a[iMin];
-//   a[iMin] = temp;
-// }
-
-// NOTE: bubble sort
-// for (let i=0;i<a.length-1;i++){
-//   for (let j=0;j<a.length-(i+1);j++){
-//     if(a[j]>a[j+1]){
-//       let temp = a[j];
-//       a[j] = a[j+1];
-//       a[j+1] = temp;
-//     }
-//   }
-// }
-
-// NOTE: merge sort
-
-/*
-let tempA = [];
-let newA = [];
-while (a.length>0){
-  newA = a.slice(0,2);
-  a.splice(0,2);
-  console.log(newA, a);
-  if(newA[0]>newA[1]){
-    let temp = newA[0];
-    newA[0] = newA[1];
-    newA[1] = temp;
-  }
-}
-*/
-
-// TODO: need to update for non-even numbers
-// var iterations = Math.log2(randomArray.length);
-
-// for (let i=0;i<iterations;i++){
-//   let temp1 = [];
-//   let temp2 = [];
-//   let newArray = [];
-//   newArray.push(a.slice(0,a.length/2), a.slice(a.length/2));
-//   // console.log(newArray); // working!
-//   a = newArray;
-// }
-
-
 
 // NOTE: breaking down the array
-const divide = function (array) {
-  console.log(array);
+const divide = function (array, runs) {
+  // console.log(array);
   let arrA = [];
   let arrB = [];
   arrA = array.slice(0,array.length/2);
   arrB = array.slice(array.length/2);
 
-  console.log(arrA);
-  console.log(arrB);
-
-  if (arrA.length>1) {
-    // let newA = divide(arrA);
-    arrA = divide(arrA);
-    // let newB = divide(arrB);
-    arrB = divide(arrB);
-  } else {
-    return [arrA,arrB];
-  }
-
+  return [arrA,arrB];
 }
-
 
 // NOTE: joining two arrays
 const combine = function (a, b){
   let newArray = [];
   while ((a.length>0) || (b.length>0)) {
-    console.log("a = " + a, "b = " + b);
-    console.log("a[0] = " + a[0], "b[0] = " + b[0]);
+    // console.log("a = " + a, "b = " + b);
+    // console.log("a[0] = " + a[0], "b[0] = " + b[0]);
     if (a[0] <= b[0] || b.length === 0) {
       newArray.push(a[0]);
       a.splice(0,1);
@@ -98,17 +24,30 @@ const combine = function (a, b){
       b.splice(0,1);
     }
   }
-  console.log(newArray);
+  // console.log(newArray);
+  return newArray;
 }
 
 const mergeSort = function(array){
-  var iterations = Math.log2(array);
-
-  var tempArray = divide(array);
-  combine(tempArray);
+  var temp = divide(array);
+  // console.log(temp[0],temp[1]);
+  if (temp[0].length>1){
+      temp[0] = mergeSort(temp[0]);
+      temp[1] = mergeSort(temp[1]);
+  }
+  let newArray = combine(temp[0],temp[1]);
+  return newArray;
 }
 
+let arr1 = randomArray.slice();
+let arr2 = randomArray.slice();
+let arr3 = randomArray.slice();
+selectionSort(arr1);
+bubbleSort(arr2);
 
-// console.timeEnd("factorial test");
-
-// console.log(randomArray);
+// console.log("Given Array: ", arr3);
+console.time("factorial test");
+let sortedArray = mergeSort(randomArray);
+console.log("<Merge Sort>");
+// console.log("Sorted Array: ", sortedArray);
+console.timeEnd("factorial test");
--- sorting.js
+++ sorting.js
@@ -1,13 +1,11 @@
-const randomArray = Array.from({length: 512}, () => Math.floor(Math.random() * 1000) + 1);
+const randomArray = Array.from({length: 2048}, () => Math.floor(Math.random() * 1000) + 1);
 
 // console.log("Random Array = ", randomArray);
 
 var givenArray = [5,3,6,1,2,4,8,7];
 
-
-// NOTE: selection sort
 const selectionSort = function (a) {
-  console.log("Given Array: ", a);
+  // console.log("Given Array: ", a);
   console.time("factorial test");
   for (let i=0;i<a.length;i++){
     let iMin = i;
@@ -21,13 +19,12 @@ const selectionSort = function (a) {
     a[iMin] = temp;
   }
   console.log("<Selection Sort>");
-  console.log("Sorted Array: ", a);
+  // console.log("Sorted Array: ", a);
   console.timeEnd("factorial test");
 }
 
-// NOTE: bubble sort
 const bubbleSort = function (a) {
-  console.log("Given Array: ", a);
+  // console.log("Given Array: ", a);
   console.time("factorial test");
   for (let i=0;i<a.length-1;i++){
     for (let j=0;j<a.length-(i+1);j++){
@@ -39,61 +36,63 @@ const bubbleSort = function (a) {
     }
   }
   console.log("<Bubble Sort>");
-  console.log("Sorted Array: ", a);
+  // console.log("Sorted Array: ", a);
   console.timeEnd("factorial test");
 };
 
-// NOTE: merge sort
+const mergeSort = function (a) {
+  // console.log("Given Array: ", a);
+  console.time("factorial test");
 
-// NOTE: breaking down the array
-const divide = function (array, runs) {
-  // console.log(array);
-  let arrA = [];
-  let arrB = [];
-  arrA = array.slice(0,array.length/2);
-  arrB = array.slice(array.length/2);
+  const divide = function (array, runs) {
+    // console.log(array);
+    let arrA = [];
+    let arrB = [];
+    arrA = array.slice(0,array.length/2);
+    arrB = array.slice(array.length/2);
 
-  return [arrA,arrB];
-}
+    return [arrA,arrB];
+  }
 
-// NOTE: joining two arrays
-const combine = function (a, b){
-  let newArray = [];
-  while ((a.length>0) || (b.length>0)) {
-    // console.log("a = " + a, "b = " + b);
-    // console.log("a[0] = " + a[0], "b[0] = " + b[0]);
-    if (a[0] <= b[0] || b.length === 0) {
-      newArray.push(a[0]);
-      a.splice(0,1);
-    } else {
-      newArray.push(b[0]);
-      b.splice(0,1);
+  const combine = function (a, b){
+    let newArray = [];
+    while ((a.length>0) || (b.length>0)) {
+      // console.log("a = " + a, "b = " + b);
+      // console.log("a[0] = " + a[0], "b[0] = " + b[0]);
+      if (a[0] <= b[0] || b.length === 0) {
+        newArray.push(a[0]);
+        a.splice(0,1);
+      } else {
+        newArray.push(b[0]);
+        b.splice(0,1);
+      }
     }
+    // console.log(newArray);
+    return newArray;
   }
-  // console.log(newArray);
-  return newArray;
-}
 
-const mergeSort = function(array){
-  var temp = divide(array);
-  // console.log(temp[0],temp[1]);
-  if (temp[0].length>1){
-      temp[0] = mergeSort(temp[0]);
-      temp[1] = mergeSort(temp[1]);
+  const merge = function(array){
+    var temp = divide(array);
+    // console.log(temp[0],temp[1]);
+    if (temp[0].length>1){
+        temp[0] = merge(temp[0]);
+        temp[1] = merge(temp[1]);
+    }
+    let newArray = combine(temp[0],temp[1]);
+    return newArray;
   }
-  let newArray = combine(temp[0],temp[1]);
-  return newArray;
-}
+
+  let sortedArray = merge(a);
+  console.log("<Merge Sort>");
+  // console.log("Sorted Array: ", sortedArray);
+  console.timeEnd("factorial test");
+  // console.log(sortedArray);
+};
+
 
 let arr1 = randomArray.slice();
 let arr2 = randomArray.slice();
 let arr3 = randomArray.slice();
 selectionSort(arr1);
 bubbleSort(arr2);
-
-console.log("Given Array: ", arr3);
-console.time("factorial test");
-let sortedArray = mergeSort(randomArray);
-console.log("<Merge Sort>");
-console.log("Sorted Array: ", sortedArray);
-console.timeEnd("factorial test");
+mergeSort(arr3);
